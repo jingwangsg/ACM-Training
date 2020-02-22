282b79f4aca76058e89c6fb8bf6149f8a03b8c5f
kningtg <kningtg@gmail.com>
2020-02-22 16:09:18 +0800

    HDU1043 finished

--- HDU/1043/main.cpp
+++ HDU/1043/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <cstring>
 #include <queue>
 #include <algorithm>
 #include <vector>
 #include <cmath>
+#include <cstdlib>
 using namespace std;
 
 int target_seq[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -17,18 +19,19 @@ int init_pos;
 string ans;
 
 int Cantor(int *seq);
-void InvCantor(int state, int *seq);
-int calc_h(int state);
+int calc_h(int *seq);
 
 struct SeqItem {
     SeqItem(){}
-    SeqItem(int state_, int pos_, int g_, string path_) {
-        this->state = state_;
+    SeqItem(int *seq_, int pos_, int g_, string path_) {
+        for (int i=0; i<9; i++) this->seq[i] = seq_[i];
         this->pos = pos_;
+        this->state = Cantor(this->seq);
         this->g = g_;
-        this->h = calc_h(state_);
+        this->h = calc_h(this->seq);
         this->path = path_;
     }
+    int seq[9];
     int state;
     //h - current cost, g - predicted cost
     int g, h;
@@ -39,7 +42,7 @@ struct SeqItem {
     }
 };
 
-priority_queue <SeqItem> q;
+
 
 int Cantor(int *seq) {
     int cnt=0;
@@ -52,21 +55,8 @@ int Cantor(int *seq) {
     return ret;
 }
 
-void InvCantor(int state, int *seq) {
-    vector <int> rest_num;
-    for (int i=0; i<9; i++) rest_num.push_back(i);
-    for (int i=0; i<9; i++) {
-        int rank_in_rest = state / factorial[8-i];
-        state %= factorial[8-i];
-        sort(rest_num.begin(), rest_num.end());
-        seq[i] = rank_in_rest + 1;
-    }
-}
-
-int calc_h(int state){
+int calc_h(int *seq){
     int ret = 0;
-    int seq[9];
-    InvCantor(state, seq);
     for (int i=0; i<9; i++) {
         int r = i/3, c = i%3;
         int tr = (seq[i]-1)/3, tc = (seq[i]-1)%3;
@@ -79,38 +69,39 @@ int calc_h(int state){
 bool NoSolution() {
     int invs = 0;
     for (int i=0; i<9; i++) {
+        if (init_seq[i] == 9) continue;
         for (int j=0; j<i; j++) {
+            if (init_seq[j] == 9) continue;
             if (init_seq[i] < init_seq[j]) invs ++;
         }
     }
-    return !(invs & 1);
+    return invs&1;
 }
 
 void AStar() {
-    while (!q.empty()) q.pop();
-    q.push(SeqItem(init_state, init_pos, 0, ""));
-    int cur_seq[9];
+    priority_queue <SeqItem> q;
+    memset(visited, 0, sizeof(visited));
+    q.push(SeqItem(init_seq, init_pos, 0, ""));
     while (!q.empty()) {
         SeqItem cur_item = q.top(); q.pop();
         int cur_state = cur_item.state;
         visited[cur_state] = 1;
         int cur_pos = cur_item.pos;
-        int cur_g;
-        InvCantor(cur_state, cur_seq);
         for (int i=0; i<4; i++) {
             int next_pos = cur_pos + mov[i];
+            if (abs(mov[i]) == 1 && next_pos / 3 != cur_pos / 3) continue;
             if (next_pos < 0 || next_pos > 8) continue;
-            swap(cur_seq[cur_pos], cur_seq[next_pos]);
-            int next_state = Cantor(cur_seq);
-            swap(cur_seq[cur_pos], cur_seq[next_pos]);
+            swap(cur_item.seq[cur_pos], cur_item.seq[next_pos]);
+            int next_state = Cantor(cur_item.seq);
             if (!visited[next_state]) {
-                SeqItem next_item(next_state, next_pos, cur_item.g+1, cur_item.path+mov_ch[i]);
+                SeqItem next_item(cur_item.seq, next_pos, cur_item.g+1, cur_item.path+mov_ch[i]);
                 if (next_state == target_state) {
                     ans = next_item.path;
                     return;
                 }
                 q.push(next_item);
             }
+            swap(cur_item.seq[cur_pos], cur_item.seq[next_pos]);
         }
     }
 }
@@ -126,23 +117,31 @@ int main() {
         tmp *= i;
         factorial[i] = tmp;
     }
-
-    for (int i=0; i<9; i++) {
-        char ch;
-        cin >> ch;
+    char ch;
+    while (cin >> ch) {
+        //read first char to make sure the input is not end
         if (ch == 'x'){
-            init_seq[i] = 9;
-            init_pos = i;
+            init_seq[0] = 9;
+            init_pos = 0;
         }
-        else init_seq[i] = ch - '0';
-    }
+        else init_seq[0] = ch - '0';
 
-    target_state = Cantor(target_seq);
-    if (!NoSolution()) {
-        AStar();
-        cout << ans;
-    } else {
-        cout << "unsolvable";
+        for (int i=1; i<9; i++) {
+            cin >> ch;
+            if (ch == 'x'){
+                init_seq[i] = 9;
+                init_pos = i;
+            }
+            else init_seq[i] = ch - '0';
+        }
+
+        target_state = Cantor(target_seq);
+        if (!NoSolution()) {
+            AStar();
+            cout << ans << endl;
+        } else {
+            cout << "unsolvable" << endl;
+        }
     }
     return 0;
 }
